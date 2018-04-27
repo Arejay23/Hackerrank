963713d36f1088280b33a5ed2a7593061b4efedb
Rajat Jain <rajat78711@gmail.com>
2018-04-27 22:11:18 +0530

    Create solution.c

---
+++ 30 days of code/Day 3- Intro to Conditional Statements/solution.c
@@ -0,0 +1,33 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int n; 
+    scanf("%d",&n);
+    if(n%2==0)
+        {
+        if(n>=2)
+            {if(n<=5) 
+            {
+            printf("Not Weird");
+        }
+             if(n>20)
+                 {
+                 printf("Not Weird");
+             }
+    }
+    if(n>=6)
+        {
+        if(n<=20)
+            {{printf("Weird");}}
+    }
+    }
+    else
+        {printf("Weird");}
+    return 0;
+}
