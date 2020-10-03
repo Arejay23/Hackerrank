4596e20b15391312a21546befb22fc463ef10e56
Nikhil Jha <exilednick@outlook.com>
2020-10-03 13:17:03 +0530

    optimised day 29

---
+++ 30 days of code/Day 29- Bitwise AND/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int t,n,k;
+	cin>>t;
+	while(t--) {
+		cin>>n>>K;
+		
+		if((k-1)|k <=n )
+			cout<<k-1;
+		else
+			cout<<k-2;
+			
+		cout<<"\n";
+	}
+}
